30d826e82d47cca2f3456571fc05b02288b76b0b
bruno <brunorochalp3@gmail.com>
2025-07-28 12:43:34 -0300

    Corrigir renderização e ajustar homepage para .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import PassengerHome from './components/PassengerHome';
 
 function App() {
   return (
-    <div>Teste Autobix</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/passenger-home" element={<PassengerHome />} />
+      </Routes>
+    </Router>
   );
 }
 
